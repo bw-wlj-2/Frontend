27a8e9b9f54f81ad11be96ac0d5b2c489e718324
Jonathan Hanson <jhanson8@uoregon.edu>
2020-02-04 13:17:22 -0800

    dashboard comp and action updates

--- my-app/src/components/Dashboard.js
+++ my-app/src/components/Dashboard.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { connect } from "react-redux";
+import { fetchUser } from "../actions/UserActions/FetchUser";
+import { fetchExercises } from "../actions/UserActions/FetchExercises";
 
 const Dashboard = props => {
+  const userID = localStorage.getItem("userID");
+  const history = useHistory();
+
+  const [exercises, setExercises] = useState();
+
+  console.log("checking for exercise", exercises);
+
+  useEffect(() => {
+    props.fetchUser(userID);
+  }, []);
+  console.log("this is props", props);
+
+  useEffect(() => {
+    props.fetchExercises(userID);
+    setExercises(props.userExercises);
+  }, [exercises]);
+  console.log("this is props", props);
+
+  const editProfile = event => {
+    event.preventDefault();
+    history.push("/editprofile");
+  };
+
   return (
     <div>
       <p>Dashboard</p>
     </div>
   );
 };
-export default Dashboard;
+
+const mapStateToProps = state => {
+  return state;
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchUser, fetchExercises }
+)(Dashboard);
